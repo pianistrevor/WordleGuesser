d1b2ee9ba672ee52dec0dbe12fd74f7a5b5fabc3
natitr <trevor.natiuk@wallawalla.edu>
2022-02-08 00:17:44 -0800

    Large-scale refactor of storage and elimination

--- guesser.py
+++ guesser.py
@@ -1,9 +1,10 @@
-import numpy
+from heapq import heappop, heapify
 from random import randint
+import sys
 
-###########
-# Globals #
-###########
+##############
+# Unchanging #
+##############
 
 common = ['E', 'A', 'R', 'I', 'O', 'T', 'N', 'S', 'L', 'C', 'U', 'D', 'P', 'M', 'H', 'G', 'B', 'F', 'Y', 'W', 'K', 'V', 'X', 'Z', 'J', 'Q']
 starting_guesses = ['ARISE', 'TASER', 'PAINT', 'SWING', 'FRAME']
@@ -12,88 +13,68 @@ starting_guesses = ['ARISE', 'TASER', 'PAINT', 'SWING', 'FRAME']
 # Helper functions #
 ####################
 
-def generate_perms(guess_string, permute_list, solutions):
-    """ Generates permuted strings using the 'Y' characters """
-    if len(permute_list) == 0:
-        solutions.append(guess_string.copy())
-        return
-    i = 0
-    while i < 5:
-        if guess_string[i] != '-':
-            i += 1
-        else:
-            if cantbe[i].count(permute_list[0]) > 0:
-                # This letter can't be here since it must be permuted
-                i += 1
-                continue
-            guess_string[i] = permute_list[0]
-            permute_list = permute_list[1:]
-            # Recursive call to place rest of permute list
-            generate_perms(guess_string, permute_list, solutions)
-            # Add back to the list
-            permute_list.insert(0, guess_string[i])
-            guess_string[i] = '-'
-            i += 1
-            
-def generate_guess(guess_string, letter_list, solution):
-    """ Generates solution strings using the rest of guess_letters """
-    if guess_string.count('-') == 0:
-        # Check if the solution exists in str_array
-        ints = list(ord(char) - 65 for char in guess_string)
-        if (str_array[ints[0]][ints[1]][ints[2]][ints[3]][ints[4]].tolist()) == 1:
-            solution.append(guess_string.copy())
-        return
-    # Search for next blank character
-    i = guess_string.index('-')
-    j = 0
-    while j < len(letter_list):
-        if cantbe[i].count(letter_list[j]) > 0:
-            # This letter can't be here since it must be permuted
-            j += 1
-            continue
-        guess_string[i] = letter_list[j] # Most common first
-        generate_guess(guess_string, letter_list, solution)
-        guess_string[i] = '-' # Exhausted recursive guesses
-        j += 1
+def filter_heap(heap, guess_tuple, color_string):
+    new_heap = []
+    for word_tuple in heap:
+        if fits_template(color_string, guess_tuple, word_tuple):
+            new_heap.append(word_tuple)
+    heapify(new_heap)
+    return new_heap
+
+def fits_template(color_string, guess_tuple, word_tuple):
+    for i in range(5):
+        # Case 1: it's green
+        if color_string[i] == 'G' and word_tuple[1][i] != guess_tuple[1][i]:
+            return False
+        # Case 2: it's yellow
+        elif color_string[i] == 'Y':
+            # Must be present and not in its original spot
+            if word_tuple[1].count(guess_tuple[1][i]) < 1 or cantbe[i].count(word_tuple[1][i]) > 0:
+                return False
+        # Case 3: it's grey
+        elif color_string[i] == '-':
+            # Must not be present
+            if cantbe[i].count(word_tuple[1][i]) > 0:
+                return False
+    return True
 
 ##################
 # Initialization #
 ##################
 
-guess = list((char for char in starting_guesses[randint(0, 4)]))
-guess_int = list(ord(char) - 65 for char in guess)
-next_guess = ['-', '-', '-', '-', '-']
+#guess = (-1, starting_guesses[randint(0, 4)]) # Unused weight
+guess = (-1, 'TASER')
+next_guess = ''
 permute_list = list()           # List of letters to permute
-permstring_list = list()        # List of strings with permuted letters
-solution_list = list()          # List of candidate solutions
 cantbe = [[], [], [], [], []]   # List of letters at each index that were 'Y'
 colorstr = ''
-guess_letters = common          # Initialize with all 26 letters
 attempts = 1
 
-# Generate 5-dimensional row-major array of words (will be sparsely populated)
-# Saves time compared to a BFS or DFS
-str_array = numpy.ndarray(shape=(26, 26, 26, 26, 26), dtype=numpy.ubyte, order='C')
+# Generate heap (priority queue) of all words based on commonality weight
+word_list = []
 with open('words.txt', 'r') as file:
     for line in file:
-        ints = list((ord(line[i]) - 65 for i in range(0,5)))
-        str_array[ints[0]][ints[1]][ints[2]][ints[3]][ints[4]] = 1
+        weight = (common.index(line[i]) + 1 for i in range(0,5))
+        word_list.append((sum(weight), line[0:5]))
+heapify(word_list)
+        
         
 ################
 #     Main     #
 ################
 
 while attempts <= 6:
-    print("".join(guess))
+    print(guess[1])
     while len(colorstr) != 5 and colorstr != 'r':
+        if len(word_list) == 0:
+            print("We ran out of words? Something went wrong...")
+            sys.exit()
         colorstr = input('>> ')
         if colorstr == 'r':
             if attempts != 1:
                 # New solution
-                guess = solution_list.pop()
-                guess_int = list(ord(char) - 65 for char in guess)
-                # Remove new guess from solutions
-                str_array[guess_int[0]][guess_int[1]][guess_int[2]][guess_int[3]][guess_int[4]] = 0
+                guess = heappop(word_list)
+                print(guess[1])
             else:
                 print("You cannot reject the first solution.")
             colorstr = ''
@@ -103,32 +84,28 @@ while attempts <= 6:
     for i, color in enumerate(colorstr):
         # Green: lock it in place (don't change it)
         if color == 'G':
-            next_guess[i] = guess[i]
+            next_guess += 'G'
         # Yellow: add to cantbe, add to permute_list, set to '-'
         elif color == 'Y':
-            permute_list.append(guess[i])
-            cantbe[i].append(guess[i])
-            next_guess[i] = '-'
-        # Grey: remove from letters to guess, set to '-'
+            permute_list.append(guess[1][i])
+            cantbe[i].append(guess[1][i])
+            next_guess += '-'
+        # Grey: add to cantbe if not a yellow elsewhere, set to '-'
         else:
-            next_guess[i] = '-'
-            if guess_letters.count(guess[i]) != 0:
-                guess_letters.remove(guess[i])
-    # Only get permstrings if there's at least one letter to permute
-    if len(permute_list) > 0:
-        generate_perms(next_guess, permute_list, permstring_list)
-    else:
-        permstring_list.append(next_guess)
-    # Generate solutions
-    solution_list.clear()
-    for string in permstring_list:
-        generate_guess(string, guess_letters, solution_list)
-    # Just take the first generated solution - likely close to most common
-    guess = solution_list[0]
-    guess_int = list(ord(char) - 65 for char in guess)
+            next_guess += '-'
+            # Only goes in ALL cantbes if not in permute_list
+            # Else only in the current one
+            if guess[1][i] not in permute_list:
+                for j in range(5):
+                    cantbe[j].append(guess[1][i])
+            else:
+                cantbe[i].append(guess[1][i])
+    # Now eliminate words from heap
+    print(cantbe)
+    word_list = filter_heap(word_list, guess, colorstr)
+    guess = heappop(word_list)
     # Re-initialize
     permute_list.clear()
-    permstring_list.clear()
     colorstr = ''
     attempts += 1
 
